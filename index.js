35dd019baa70ce00c06a5b1633fb00f2c97fd5c4
prabin <bhandariprabin402@gmail.com>
2023-12-19 17:24:06 +0545

    add j

---
+++ index.js
@@ -0,0 +1,9 @@
+console log("hello world");
+
+var person ="prabin";
+// let allow u to reassign variable
+let person1 ="ram";
+person1="prabin";
+// not allow to change variable
+//const person 2="shyam";
+console log("person1");
